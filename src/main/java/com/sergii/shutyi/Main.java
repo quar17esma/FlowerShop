840798c32c7d9a467c651f865aee24e2fa58f054
quar17esma <quar17esma@gmail.com>
2017-07-27 12:08:44 +0300

    add consructor

--- src/main/java/com/sergii/shutyi/Main.java
+++ src/main/java/com/sergii/shutyi/Main.java
@@ -1,11 +1,15 @@
 package com.sergii.shutyi;
 
-/**
- *
- *
- */
+import com.sergii.shutyi.model.Model;
+import com.sergii.shutyi.view.View;
+import com.sergii.shutyi.сontroller.Controller;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        // Initialization
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
     }
 }
--- src/main/java/com/sergii/shutyi/сontroller/Controller.java
+++ src/main/java/com/sergii/shutyi/сontroller/Controller.java
@@ -1,4 +1,15 @@
 package com.sergii.shutyi.сontroller;
 
+import com.sergii.shutyi.model.Model;
+import com.sergii.shutyi.view.View;
+
 public class Controller {
+    Model model;
+    View view;
+
+    // Constructor
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
 }
