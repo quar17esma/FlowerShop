67c554d7addaef3b8123be98b67d741a8475895d
quar17esma <quar17esma@gmail.com>
2017-07-27 12:07:56 +0300

    add getters and setters

--- src/main/java/com/sergii/shutyi/model/entity/Bouquet.java
+++ src/main/java/com/sergii/shutyi/model/entity/Bouquet.java
@@ -22,4 +22,86 @@ public class Bouquet {
     Base base;
 
     ArrayList<Flower> flowers;
+
+
+    //getters and setters
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
+
+    public Use getUse() {
+        return use;
+    }
+
+    public void setUse(Use use) {
+        this.use = use;
+    }
+
+    public Stems getStems() {
+        return stems;
+    }
+
+    public void setStems(Stems stems) {
+        this.stems = stems;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Freshness getFreshness() {
+        return freshness;
+    }
+
+    public void setFreshness(Freshness freshness) {
+        this.freshness = freshness;
+    }
+
+    public Wrapper getWrapper() {
+        return wrapper;
+    }
+
+    public void setWrapper(Wrapper wrapper) {
+        this.wrapper = wrapper;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
+
+    public Tape getTape() {
+        return tape;
+    }
+
+    public void setTape(Tape tape) {
+        this.tape = tape;
+    }
+
+    public Base getBase() {
+        return base;
+    }
+
+    public void setBase(Base base) {
+        this.base = base;
+    }
+
+    public ArrayList<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(ArrayList<Flower> flowers) {
+        this.flowers = flowers;
+    }
 }
--- src/main/java/com/sergii/shutyi/model/entity/plant/Flower.java
+++ src/main/java/com/sergii/shutyi/model/entity/plant/Flower.java
@@ -1,6 +1,22 @@
 package com.sergii.shutyi.model.entity.plant;
 
 public class Flower extends Plant {
-    public boolean isFeedingNeed;
-    public boolean isSupportNeed;
+    private boolean isFeedingNeed;
+    private boolean isSupportNeed;
+
+    public boolean isFeedingNeed() {
+        return isFeedingNeed;
+    }
+
+    public void setFeedingNeed(boolean feedingNeed) {
+        isFeedingNeed = feedingNeed;
+    }
+
+    public boolean isSupportNeed() {
+        return isSupportNeed;
+    }
+
+    public void setSupportNeed(boolean supportNeed) {
+        isSupportNeed = supportNeed;
+    }
 }
